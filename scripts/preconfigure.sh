7049c79818a5b6af2b6a479c978fb298bebe21cf
Yiran Zhou <zdyxry@gmail.com>
2025-05-24 13:15:03 +0800

    chore: disable auto suspend

--- scripts/preconfigure.sh
+++ scripts/preconfigure.sh
@@ -2,5 +2,10 @@
 
 set -ouex pipefail
 
+# Disable auto suspend
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+
 # Apply IP Forwarding before installing Docker to prevent messing with LXC networking
 sysctl -p
+
