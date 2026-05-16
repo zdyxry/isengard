a1faa81743202dc2e8c0b10e834bad2811822782
zdyxry <zdyxry@gmail.com>
2026-05-16 12:38:58 +0800

    fix: clean stale rpm-ostree metadata cache before install
    Terra repo repodata checksum has changed, but bluefin-dx base image
    caches the old one. Add 'rpm-ostree cleanup -m' to force re-fetch
    latest repodata before installing packages.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -11,5 +11,8 @@ packages=(
   ${sysadmin_packages[@]}
 )
 
+# Clean stale metadata cache before installing, in case upstream repo repodata has changed
+rpm-ostree cleanup -m
+
 # install rpms
 rpm-ostree install ${packages[@]}
